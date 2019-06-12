54d55b744113a487fe1818a85820d3a584b301d4
dnzlike <dnz7993@sjtu.edu.cn>
2019-06-12 14:57:37 +0800

    add correlation

--- Scripts/WebTours/pre_cci.c
+++ Scripts/WebTours/pre_cci.c
@@ -2610,6 +2610,18 @@ void
 
 vuser_init()
 {
+	
+	 
+	web_reg_save_param_attrib(
+		"ParamName=userSession",
+		"TagName=input",
+		"Extract=value",
+		"Name=userSession",
+		"Type=hidden",
+		"SEARCH_FILTERS",
+		"IgnoreRedirections=No",
+		"RequestUrl=*/nav.pl*",
+		"LAST");
 
 	lr_start_transaction("login");
 
@@ -2627,13 +2639,20 @@ vuser_init()
 
 	lr_think_time(5);
 
-	web_submit_form("login.pl",
+	web_submit_data("login.pl",
+		"Action=http://127.0.0.1:1080/cgi-bin/login.pl",
+		"Method=POST",
+		"RecContentType=text/html",
+		"Referer=http://127.0.0.1:1080/cgi-bin/nav.pl?in=home",
 		"Snapshot=t60.inf",
+		"Mode=HTML",
 		"ITEMDATA",
+		"Name=userSession", "Value={userSession}", "ENDITEM",
 		"Name=username", "Value={username}", "ENDITEM",
 		"Name=password", "Value=123456", "ENDITEM",
-		"Name=login.x", "Value=50", "ENDITEM",
-		"Name=login.y", "Value=16", "ENDITEM",
+		"Name=JSFormSubmit", "Value=on", "ENDITEM",
+		"Name=login.x", "Value=58", "ENDITEM",
+		"Name=login.y", "Value=13", "ENDITEM",
 		"LAST");
 
 	lr_end_transaction("login", 2);
